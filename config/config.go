ce0a59927803152834e9615edfcb6891daf5a76b
ys <yoshi.23unog34@gmail.com>
2021-02-23 12:08:01 +0900

    update: config

--- config/config.go
+++ config/config.go
@@ -1,18 +1,20 @@
 package config
 
 import (
-	"fmt"
 	"os"
 )
 
-// DBDSN is returned data source name to connect database
-func DBDSN() string {
-	return fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_DATABASE"),
-	) + "?parseTime=true&collation=utf8mb4_bin"
+// IsLocal はローカル環境がどうか返します。
+func IsLocal() bool {
+	return os.Getenv("ENV") == "local"
+}
+
+// IsDev はDev環境がどうか返します。
+func IsDev() bool {
+	return os.Getenv("ENV") == "dev"
+}
+
+// IsPrd はPrd環境がどうか返します。
+func IsPrd() bool {
+	return os.Getenv("ENV") == "prod"
 }
