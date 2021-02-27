4a348c04c27fce286efe342e14485127f8311013
ys <yoshi.23unog34@gmail.com>
2021-02-27 23:19:04 +0900

    update: main.go for server start

--- main.go
+++ main.go
@@ -3,20 +3,29 @@ package main
 import (
 	"fmt"
 
-	"github.com/YoshitakaSS/go_auth/infra"
+	"github.com/YoshitakaSS/go_auth/config"
+	"github.com/YoshitakaSS/go_auth/web"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	dbMap, err := infra.InitDB()
+	// 明示的に環境変数を読み込み
+	err := godotenv.Load()
 	if err != nil {
-		fmt.Println(err)
+		panic("Error loading .env file")
 	}
-	defer dbMap.Db.Close()
+	// dbMap, err := infra.InitDB()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// defer dbMap.Db.Close()
 
-	redis, err := infra.InitRedis()
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer redis.Close()
+	// redis, err := infra.InitRedis()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// defer redis.Close()
 
+	s := web.NewServer()
+	s.Start(fmt.Sprintf("%s:%s", config.HostName(), config.Port()))
 }
