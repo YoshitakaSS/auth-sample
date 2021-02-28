4a6d950f9049e55b573c023b60324cfa32a6ac1a
ys <yoshi.23unog34@gmail.com>
2021-02-28 12:13:33 +0900

    fix: dsn config

--- config/dsn.go
+++ config/dsn.go
@@ -10,10 +10,10 @@ func DBDSN() string {
 	return fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s",
 		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_PASS"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
-		os.Getenv("DB_DATABASE"),
+		os.Getenv("DB_NAME"),
 	) + "?parseTime=true&collation=utf8mb4_bin"
 }
 
