9d7215273799a0196d3e9127494a5938ecaa23d0
ys <yoshi.23unog34@gmail.com>
2021-03-30 21:09:56 +0900

    update: add api route

--- web/route.go
+++ web/route.go
@@ -15,5 +15,17 @@ func NewServer() *echo.Echo {
 		return c.String(http.StatusOK, "TOP")
 	})
 
+	// API Group
+	api := e.Group("/api")
+
+	// ユーザー登録
+	api.POST("/user/regsiter", func(c echo.Context) error {
+		return c.String(http.StatusOK, "ユーザー登録用Route")
+	})
+	// ユーザーログイン
+	api.POST("/user/login", func(c echo.Context) error {
+		return c.String(http.StatusOK, "ユーザーログイン用Route")
+	})
+
 	return e
 }
