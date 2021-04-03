6822b9e485b5a95dbb479573bb1e61d8a20de80e
ys <yoshi.23unog34@gmail.com>
2021-04-03 10:24:11 +0900

    add: user vo

--- domain/entity/user.go
+++ domain/entity/user.go
@@ -1,10 +1,21 @@
 package entity
 
+import "github.com/YoshitakaSS/go_auth/domain/vo"
+
 // User はユーザー情報を表す構造体
 type User struct {
 	Email     string
-	UserName  string
-	BirthDate string
+	userName  *vo.UserName
+	birthDate *vo.UserBirthDate
 	Password  string
 	Gender    int
+}
+
+// NewUser はUserEntityを返します
+func NewUser(u *vo.UserName, b *vo.UserBirthDate) (*User, error) {
+
+	return &User{
+		userName:  u,
+		birthDate: b,
+	}, nil
 }
