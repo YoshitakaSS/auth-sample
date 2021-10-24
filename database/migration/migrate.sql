2fa99e1451069ef2770f3900e3c31a76404433d5
ys <yoshi.23unog34@gmail.com>
2021-10-24 10:22:57 +0900

    Fix: migration

--- database/migration/migrate.sql
+++ database/migration/migrate.sql
@@ -1,12 +1,12 @@
 DROP TABLE `users` IF EXISTS `users`
 
 CREATE TABLE `users` (
-  `user_id` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'ユーザID (不変)',
-  `user_name` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'ユーザー名',
+  `user_id` bigint COLLATE utf8mb4_bin NOT NULL COMMENT 'ユーザID',
+  `name` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'ユーザー名',
   `email` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'メールアドレスを暗号化した文字列',
   `birth_date` date COLLATE utf8mb4_bin NOT NULL COMMENT '生年月日',
   `password` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'パスワード',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`user_id`)
   UNIQUE KEY `users_name_uindex` (`name`)
   UNIQUE KEY `users_email_uindex` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
